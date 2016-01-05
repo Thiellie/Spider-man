ef6423f8f68393386de88182024017ed2cd1beaa
Louis Thielens <louis-thielens@hotmail.com>
2016-01-05 12:34:52 +0100

    Louis is Vlaams

--- src/main/java/com/thiellie/twitchbot/Main.java
+++ src/main/java/com/thiellie/twitchbot/Main.java
@@ -4,4 +4,8 @@ package com.thiellie.twitchbot;
  * Created by Louis on 5-1-2016.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Louis is heaumeau");
+    }
 }
